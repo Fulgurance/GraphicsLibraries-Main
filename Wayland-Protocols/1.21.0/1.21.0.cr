9a6703cd52efeaa4ae1d3dc61e5771867888f319
fulgurance <zohran.londais@gmail.com>
2023-03-17 16:04:56 +0000

    Added Wayland and Wayland-Protocols

---
+++ Wayland-Protocols/1.21.0/1.21.0.cr
@@ -0,0 +1,28 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand([] of String,buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
