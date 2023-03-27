c2ee871a2234e78d018804321fb9cebd4486c413
fulgurance <zohran.londais@gmail.com>
2023-03-27 11:17:26 +0100

    Added missing ldconfig phase

--- LibXi/1.7.10/1.7.10.cr
+++ LibXi/1.7.10/1.7.10.cr
@@ -23,4 +23,10 @@ class Target < ISM::Software
         makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
     end
 
+    def install
+        super
+
+        runLdconfigCommand
+    end
+
 end
