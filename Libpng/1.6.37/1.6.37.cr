ec6a6d7904eaf7fe07d5858d5f1198c2c2b3ed4e
fulgurance <zohran.londais@gmail.com>
2023-04-05 10:17:48 +0100

    Added libpng installer

---
+++ Libpng/1.6.37/1.6.37.cr
@@ -0,0 +1,27 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--disable-static"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/libpng-1.6.37")
+        copyFile("#{buildDirectoryPath(false)}README","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/libpng-1.6.37")
+        copyFile("#{buildDirectoryPath(false)}libpng-manual.txt","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/libpng-1.6.37")
+    end
+
+end
