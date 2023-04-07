4eff6605b17784e22094de41bb2f93c57aa25589
fulgurance <zohran.londais@gmail.com>
2023-04-07 23:20:36 +0100

    Added Startup-Notification installer and improved wayland implementation

---
+++ Startup-Notification/0.12.0/0.12.0.cr
@@ -0,0 +1,30 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--disable-static"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource([path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        copyFile("#{buildDirectoryPath(false)}doc/startup-notification.txt","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/startup-notification-0.12/startup-notification.txt")
+    end
+
+    def install
+        super
+        setPermissions("#{Ism.settings.rootPath}usr/share/doc/startup-notification-0.12/startup-notification.txt",0o644)
+    end
+
+end
--- Wayland/1.19.0/1.19.0.cr
+++ Wayland/1.19.0/1.19.0.cr
@@ -17,7 +17,7 @@ class Target < ISM::Software
     def build
         super
 
-        runNinjaCommand([] of String,buildDirectoryPath)
+        runNinjaCommand(path: buildDirectoryPath)
     end
     
     def prepareInstallation
