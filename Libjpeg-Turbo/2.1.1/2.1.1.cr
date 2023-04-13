c73c1a9cee64e17d438a5ba3a621fa65cecb49ec
Fulgurance <zohran.londais@gmail.com>
2023-04-13 23:57:32 +0100

    Added Libjpeg-Turbo and Qrencode

---
+++ Libjpeg-Turbo/2.1.1/2.1.1.cr
@@ -0,0 +1,28 @@
+class Target < ISM::Software
+    
+    def configure
+        @buildDirectory = true
+        super
+
+        runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/usr",
+                            "-DCMAKE_BUILD_TYPE=RELEASE",
+                            "-DENABLE_STATIC=FALSE",
+                            "-DCMAKE_INSTALL_DOCDIR=/usr/share/doc/libjpeg-turbo-2.1.1",
+                            "-DCMAKE_INSTALL_DEFAULT_LIBDIR=lib",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
