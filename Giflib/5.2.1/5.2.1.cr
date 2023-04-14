91a47b5612a81f0856d40b00f18e7ad3568dc96b
Fulgurance <zohran.londais@gmail.com>
2023-04-14 11:06:27 +0100

    Added Giflib and Libdmx

---
+++ Giflib/5.2.1/5.2.1.cr
@@ -0,0 +1,28 @@
+class Target < ISM::Software
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["PREFIX=/usr","DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/lib/libgif.a")
+        deleteFile("#{buildDirectoryPath(false)}/doc/Makefile")
+        deleteAllFilesRecursivelyFinishing("#{buildDirectoryPath(false)}/doc",".1")
+        deleteAllFilesRecursivelyFinishing("#{buildDirectoryPath(false)}/doc",".xml")
+
+        copyDirectory("#{buildDirectoryPath(false)}/doc","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/giflib-5.2.1")
+    end
+
+    def install
+        super
+
+        setPermissions("#{Ism.settings.rootPath}usr/share/doc/giflib-5.2.1",0o755)
+    end
+
+end
