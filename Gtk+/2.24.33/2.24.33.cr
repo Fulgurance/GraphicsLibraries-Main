30983db33e3063f7c59b4e6cf004321db74c4c51
Fulgurance <zohran.londais@gmail.com>
2023-04-17 16:51:51 +0100

    Added Gtk+

---
+++ Gtk+/2.24.33/2.24.33.cr
@@ -0,0 +1,36 @@
+class Target < ISM::Software
+
+    def prepare
+        super
+
+        fileReplaceTextAtLineNumber("#{buildDirectoryPath(false)}docs/faq/Makefile.in","sgml","sgml -o gtk-faq",650)
+        fileReplaceTextAtLineNumber("#{buildDirectoryPath(false)}docs/tutorial/Makefile.in","sgml","sgml -o gtk-tut",663)
+    end
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+    def install
+        super
+
+        runGtkQueryImmodules2Command(["--update-cache"])
+    end
+
+end
