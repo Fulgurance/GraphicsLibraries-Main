95db348bf32d5ee730d389faa8a9c4dd64c9da49
Fulgurance <zohran.londais@gmail.com>
2023-04-18 23:05:32 +0100

    Added Gdk-Pixbuf

---
+++ Gdk-Pixbuf/2.42.6/2.42.6.cr
@@ -0,0 +1,36 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectoryPath = true
+        super
+    end
+    
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release",
+                            "--wrap-mode=nofallback",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+    def install
+        super
+
+        runGdkPixbufQueryLoadersCommand(["--update-cache"])
+    end
+
+end
