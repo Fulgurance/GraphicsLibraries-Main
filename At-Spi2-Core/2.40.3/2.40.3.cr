b0d9672ae3ff4cde41ed511b58350859e9ae4559
Fulgurance <zohran.londais@gmail.com>
2023-04-24 14:20:57 +0100

    Added new installers

---
+++ At-Spi2-Core/2.40.3/2.40.3.cr
@@ -0,0 +1,36 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release",
+                            "-Dsystemd_user_dir=/tmp",
+                            ".."],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+    def install
+        super
+
+        deleteFile("#{Ism.settings.rootPath}tmp/at-spi-dbus-bus.service")
+    end
+
+end
