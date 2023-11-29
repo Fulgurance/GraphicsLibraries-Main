610235b210f3bde2c4e82b574b3a56a02c660890
Fulgurance <zohran.londais@gmail.com>
2023-11-29 11:16:45 +0000

    Added new software

---
+++ Libxcvt/0.1.2/0.1.2.cr
@@ -0,0 +1,33 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+
+        runMesonCommand(["setup",@buildDirectoryName],mainWorkDirectoryPath)
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--localstatedir=/var",
+                            "--disable-static",
+                            "--buildtype=release"],
+                            mainWorkDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
