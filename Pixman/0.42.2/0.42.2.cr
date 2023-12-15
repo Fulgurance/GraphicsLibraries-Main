3a624ec9dc8fef775c5618e62cb89234e0cf39ff
Fulgurance <zohran.londais@gmail.com>
2023-12-15 19:06:40 +0000

    Patched syntax error

--- Pixman/0.42.2/0.42.2.cr
+++ Pixman/0.42.2/0.42.2.cr
@@ -4,7 +4,7 @@ class Target < ISM::Software
         @buildDirectory = true
         super
 
-        runMesonCommand(["setup",@buildDirectoryNames[:mainBuild],mainWorkDirectoryPath)
+        runMesonCommand(["setup",@buildDirectoryNames[:mainBuild]],mainWorkDirectoryPath)
     end
 
     def configure
