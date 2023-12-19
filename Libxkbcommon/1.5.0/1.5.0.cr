72db84946e2cd4151eb4f5aec9e28465e96ef2de
Fulgurance <zohran.londais@gmail.com>
2023-12-19 21:22:30 +0000

    Added missing argument

--- Libxkbcommon/1.5.0/1.5.0.cr
+++ Libxkbcommon/1.5.0/1.5.0.cr
@@ -4,7 +4,7 @@ class Target < ISM::Software
         @buildDirectory = true
         super
 
-        runMesonCommand(["setup",@buildDirectoryNames["MainBuild"]],mainWorkDirectoryPath)
+        runMesonCommand(["setup",@buildDirectoryNames["MainBuild"],"-Denable-docs=false"],mainWorkDirectoryPath)
     end
 
     def configure
