1c3a58bfc11652b37eeadcf85a9570df1764bb45
Fulgurance <zohran.londais@gmail.com>
2023-12-19 18:06:33 +0000

    Added missing argument

--- Gdk-Pixbuf/2.42.10/2.42.10.cr
+++ Gdk-Pixbuf/2.42.10/2.42.10.cr
@@ -4,7 +4,7 @@ class Target < ISM::Software
         @buildDirectory = true
         super
 
-        runMesonCommand(["setup",@buildDirectoryNames["MainBuild"]],mainWorkDirectoryPath)
+        runMesonCommand(["setup",@buildDirectoryNames["MainBuild"],"-Dman=false",],mainWorkDirectoryPath)
     end
     
     def configure
