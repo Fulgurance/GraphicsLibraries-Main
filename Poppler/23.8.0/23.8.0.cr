c8728e1d716c5edc73bf7a31e8e224295d1734e5
Fulgurance <zohran.londais@gmail.com>
2024-06-07 17:20:42 +0100

    Added poppler

---
+++ Poppler/23.8.0/23.8.0.cr
@@ -0,0 +1,30 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+
+        fileReplaceLineContaining("#{mainWorkDirectoryPath(false)}/tests/CMakeLists.txt","cmptest","")
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/usr",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
