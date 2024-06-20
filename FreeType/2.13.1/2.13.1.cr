3de45216940530b50ea285bf3997937920823d66
Fulgurance <zohran.londais@gmail.com>
2024-06-20 19:13:03 +0100

    Updated freetype installer

--- FreeType/2.13.1/2.13.1.cr
+++ FreeType/2.13.1/2.13.1.cr
@@ -1,20 +1,4 @@
 class Target < ISM::Software
-
-    def prepare
-        super
-
-        fileReplaceLineContaining(  path:       "#{mainWorkDirectoryPath}modules.cfg",
-                                    text:       "# AUX_MODULES += gxvalid",
-                                    newLine:    "AUX_MODULES += gxvalid")
-
-        fileReplaceLineContaining(  path:       "#{mainWorkDirectoryPath}modules.cfg",
-                                    text:       "# AUX_MODULES += otvalid",
-                                    newLine:    "AUX_MODULES += otvalid")
-
-        fileReplaceLineContaining(  path:       "#{mainWorkDirectoryPath}include/freetype/config/ftoption.h",
-                                    text:       "/* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */",
-                                    newLine:    "#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING")
-    end
     
     def configure
         super
