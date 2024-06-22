166e016e727fb7c9767daedbb827822af92553cb
Fulgurance <zohran.londais@gmail.com>
2024-06-22 21:10:30 +0100

    Updated installer

--- Cairo/1.17.6/1.17.6.cr
+++ Cairo/1.17.6/1.17.6.cr
@@ -1,23 +1,4 @@
 class Target < ISM::Software
-
-    def prepare
-        super
-
-        fileReplaceTextAtLineNumber(path:       "#{buildDirectoryPath}util/cairo-trace/lookup-symbol.c",
-                                    text:       "PTR",
-                                    newText:    "void *",
-                                    lineNumber: 109)
-
-        fileReplaceTextAtLineNumber(path:       "#{buildDirectoryPath}util/cairo-trace/lookup-symbol.c",
-                                    text:       "PTR",
-                                    newText:    "void *",
-                                    lineNumber: 112)
-
-        fileReplaceTextAtLineNumber(path:       "#{buildDirectoryPath}util/cairo-script/cairo-script-interpreter.pc.in",
-                                    text:       "ir@\n",
-                                    newText:    "ir@\nexec_prefix=@exec_prefix@\n",
-                                    lineNumber: 2)
-    end
     
     def configure
         super
