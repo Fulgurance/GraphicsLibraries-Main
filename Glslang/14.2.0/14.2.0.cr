d3c21b564bccc03f6948ec331f7df4330f2c28dc
Fulgurance <zohran.londais@gmail.com>
2024-06-23 22:56:28 +0100

    Added glslang

---
+++ Glslang/14.2.0/14.2.0.cr
@@ -0,0 +1,30 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_BUILD_TYPE=Release                                                 \
+                                    -DCMAKE_INSTALL_PREFIX=/usr                                                 \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} prefix=/usr install",
+                    path:       buildDirectoryPath)
+    end
+
+end
