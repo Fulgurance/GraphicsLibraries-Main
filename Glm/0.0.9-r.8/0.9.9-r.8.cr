f0507858f3a58c1caed7eb61e04d3d55b53f0d5c
Fulgurance <zohran.londais@gmail.com>
2024-06-26 14:36:59 +0100

    Added glm

---
+++ Glm/0.0.9-r.8/0.9.9-r.8.cr
@@ -0,0 +1,35 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runMesonCommand(arguments:  "setup --reconfigure                    \
+                                    #{@buildDirectoryNames["MainBuild"]}    \
+                                    --prefix=/usr                           \
+                                    -Dgl_provider=gl                        \
+                                    --buildtype=release",
+                        path:       mainWorkDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        runNinjaCommand(arguments:      "install",
+                        path:           buildDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+
+        deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/libGLU.a")
+    end
+
+end
