cf443670b79a39c233548db98bc530a9306f4136
Fulgurance <zohran.londais@gmail.com>
2024-06-26 18:32:05 +0100

    Updated installer

--- Glm/0.9.9-r.8/0.9.9-r.8.cr
+++ Glm/0.9.9-r.8/0.9.9-r.8.cr
@@ -1,35 +1,10 @@
 class Target < ISM::Software
-
-    def prepare
-        @buildDirectory = true
-        super
-    end
-    
-    def configure
-        super
-
-        runMesonCommand(arguments:  "setup --reconfigure                    \
-                                    #{@buildDirectoryNames["MainBuild"]}    \
-                                    --prefix=/usr                           \
-                                    -Dgl_provider=gl                        \
-                                    --buildtype=release",
-                        path:       mainWorkDirectoryPath)
-    end
-    
-    def build
-        super
-
-        runNinjaCommand(path: buildDirectoryPath)
-    end
     
     def prepareInstallation
         super
 
-        runNinjaCommand(arguments:      "install",
-                        path:           buildDirectoryPath,
-                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
-
-        deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/libGLU.a")
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/include")
+        copyDirectory("#{mainWorkDirectoryPath}/glm #{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/include/")
     end
 
 end
