c9e822434d0614c0d2a88b65861b59f33f1fd147
Fulgurance <zohran.londais@gmail.com>
2024-10-22 17:23:58 +0100

    Added new Mesa version

---
+++ Mesa/24.1.5/24.1.5.cr
@@ -0,0 +1,39 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runMesonCommand(arguments:  "setup                                          \
+                                    --reconfigure                                   \
+                                    #{@buildDirectoryNames["MainBuild"]}            \
+                                    --prefix=/usr                                   \
+                                    --buildtype=release                             \
+                                    -Dplatforms=x11,wayland                         \
+                                    -Degl-native-platform=wayland                   \
+                                    -Dgallium-drivers=\"iris,nouveau,zink,swrast\"  \
+                                    -Dvulkan-drivers=\"\"                           \
+                                    -Dvalgrind=disabled                             \
+                                    -Dlibunwind=disabled",
+                        path:       mainWorkDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        runNinjaCommand(arguments:      "install",
+                        path:           buildDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
