cab89c6a5575e76a40a51d83dbc7df90f50e276f
Fulgurance <zohran.londais@gmail.com>
2024-10-25 22:29:47 +0100

    Updated installation process

--- Mesa/24.1.5/24.1.5.cr
+++ Mesa/24.1.5/24.1.5.cr
@@ -20,7 +20,7 @@ class Target < ISM::Software
                                     -Dvalgrind=disabled                             \
                                     -Dlibunwind=disabled",
                         path:       mainWorkDirectoryPath,
-                        environment:    {"PATH" => "$PATH:/usr/lib/llvm/#{dependencyMajorVersion("@ProgrammingLanguages-Main:Llvm")}/bin"})
+                        environment:    {"PATH" => "PATH:/usr/lib/llvm/#{dependencyMajorVersion("@ProgrammingLanguages-Main:Llvm")}/bin"})
     end
     
     def build
