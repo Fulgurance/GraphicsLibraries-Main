ef72918008b331f5e9de44357197b41c9060032f
Fulgurance <zohran.londais@gmail.com>
2024-10-29 13:46:10 +0000

    Update mesa installer to set properly all drivers

--- Mesa/24.1.5/24.1.5.cr
+++ Mesa/24.1.5/24.1.5.cr
@@ -5,8 +5,73 @@ class Target < ISM::Software
         super
     end
     
+    def getSelectedGalliumDrivers
+        galliumDrivers = String.new
+
+        if option("Gallium-Driver-R300")
+            galliumDrivers += "r300,"
+        end
+        if option("Gallium-Driver-R600")
+            galliumDrivers += "r600,"
+        end
+        if option("Gallium-Driver-Radeonsi")
+            galliumDrivers += "radeonsi,"
+        end
+        if option("Gallium-Driver-Nouveau")
+            galliumDrivers += "nouveau,"
+        end
+        if option("Gallium-Driver-Virgl")
+            galliumDrivers += "virgl,"
+        end
+        if option("Gallium-Driver-Svga")
+            galliumDrivers += "svga,"
+        end
+        if option("Gallium-Driver-Swrast")
+            galliumDrivers += "swrast,"
+        end
+        if option("Gallium-Driver-Iris")
+            galliumDrivers += "iris,"
+        end
+        if option("Gallium-Driver-Crocus")
+            galliumDrivers += "crocus,"
+        end
+        if option("Gallium-Driver-I915")
+            galliumDrivers += "i915,"
+        end
+        if option("Gallium-Driver-Zink")
+            galliumDrivers += "zink,"
+        end
+
+        return galliumDrivers[0..-2]
+    end
+
+    def getSelectedVulkanDrivers
+        vulkanDrivers = String.new
+
+        if option("Vulkan-Driver-Amd")
+            vulkanDrivers += "amd,"
+        end
+        if option("Vulkan-Driver-Intel")
+            vulkanDrivers += "intel,"
+        end
+        if option("Vulkan-Driver-Intel-Hasvk")
+            vulkanDrivers += "radeonsi,"
+        end
+        if option("Vulkan-Driver-Nouveau")
+            vulkanDrivers += "nouveau,"
+        end
+        if option("Vulkan-Driver-Swrast")
+            vulkanDrivers += "swrast,"
+        end
+
+        return vulkanDrivers[0..-2]
+    end
+
     def configure
         super
+
+        galliumDrivers = getSelectedGalliumDrivers
+        vulkanDrivers = getSelectedVulkanDrivers
 
         runMesonCommand(arguments:  "setup                                          \
                                     --reconfigure                                   \
@@ -15,8 +80,8 @@ class Target < ISM::Software
                                     --buildtype=release                             \
                                     -Dplatforms=x11,wayland                         \
                                     -Degl-native-platform=wayland                   \
-                                    -Dgallium-drivers=\"iris,nouveau,zink,swrast\"  \
-                                    -Dvulkan-drivers=\"auto\"                       \
+                                    -Dgallium-drivers=\"#{galliumDrivers}\"  \
+                                    -Dvulkan-drivers=\"#{vulkanDrivers}\"                       \
                                     -Dvalgrind=disabled                             \
                                     -Dlibunwind=disabled",
                         path:       mainWorkDirectoryPath,
