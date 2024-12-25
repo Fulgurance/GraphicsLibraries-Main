065985803db8607a9b4b6edf83a1bccfc10ac914
Fulgurance <zohran.londais@gmail.com>
2024-12-25 19:16:22 +0000

    Updated prefix

--- Nv-Codec-Headers/12.2.72/12.2.72.cr
+++ Nv-Codec-Headers/12.2.72/12.2.72.cr
@@ -10,7 +10,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} PREFIX=/usr install",
                     path:       buildDirectoryPath)
     end
 
