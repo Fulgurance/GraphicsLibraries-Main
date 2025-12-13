f706680bd63e4a4ceef0b16287ca5b55b705c544
Fulgurance <zohran.londais@gmail.com>
2025-12-13 10:25:59 +0000

    Added 3.24.51 version

---
+++ Gtk/3.24.51/Installer.cr
@@ -0,0 +1,43 @@
+class Target < ISM::Software
+    
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand(arguments:  "setup --reconfigure                                                                \
+                                    #{@buildDirectoryNames["MainBuild"]}                                                \
+                                    --prefix=/usr                                                                       \
+                                    --buildtype=release                                                                 \
+                                    -Dintrospection=#{option("Gobject-Introspection") ? "true" : "false"}               \
+                                    -Dman=true                                                                          \
+                                    -Dtests=false                                                                       \
+                                    -Dbroadway_backend=true",
+                        path:       mainWorkDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(arguments:      "install",
+                        path:           buildDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+    def deploy
+        super
+
+        runGtkQueryImmodules3Command("--update-cache")
+        runGlibCompileSchemasCommand("/usr/share/glib-2.0/schemas")
+    end
+
+end
